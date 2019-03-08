75bf59359e1d6e2487401611cb0783bdccb04c14
RobinLaurenssen <r.laurenssen@student.fontys.nl>
2019-03-08 13:17:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/jquery.form.js"></script>
+  <script src="js/matchHeight/jquery.matchHeight.js"></script>
+
+
+
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+
+<div class="mainCont">
+<div class="asciCont">
+  <div class="asciTitell">
+  <pre class="asciI asci1">
+__      __   _       ____        _   
+\ \    / /  | |     |  _ \      | |  
+ \ \  / /__ | |_ ___| |_) | ___ | |_ 
+  \ \/ / _ \| __/ _ \  _ < / _ \| __|
+   \  / (_) | ||  __/ |_) | (_) | |_ 
+    \/ \___/ \__\___|____/ \___/ \__|
+                                     
+                                     
+
+  </pre>
+  <!-- end of  asci titel-->
+
+  <pre class="asciI asci2">
+ ___   ___ ___   ___  
+|__ \ / _ \__ \ / _ \ 
+   ) | | | | ) | | | |
+  / /| | | |/ /| | | |
+ / /_| |_| / /_| |_| |
+|____|\___/____|\___/ 
+                      
+                      
+
+  </pre>
+  <!-- end of  asci titel-->
+</div>
+  <div class="boomAsci">
+      <pre class="asci">
+                                                                                                          
+     ,ldddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddoc.         
+    cNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0xoc:;;:cldOXWMMMMMMMMMMMMWO'        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNOc'.           .:xXMMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk;                  'dNMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXl.                     ;0MMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMN0Okk0XWMMMMMMMMMMMMMMMMMMMMMMMMMNc                        ,KMMMMMMMN:        
+   .xMMMMMMMMMMMMMMWOc'.    .:kNMMMMMMMMMMMMMMMMMMMMMMMx.                         cNMMMMMMN:        
+   .xMMMMMMMMMMMMMNo.          :KMMMMMMMMMMMMMMMMMMMMMNc           ;cc:.          '0MMMMMMN:        
+   .xMMMMMMMMMMMMMx.     ..     lWMMMMMMMMMMMMMMMMMMMMN:          .oolx'          '0MMMMMMN:        
+   .xMMMMMMMMMMMMWd.    'd:     ;XMMMMMMMMMMMMMMMMMMMMWl           .,;.           ;XMMMMMMN:        
+   .xMMMMMMMMMMMMMNKOxoc:;..    dWMMMMMMMMMMMMMMMMMMMMM0'                        .xWMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMWX0kdoxNMMMMMMMMMMMMMMMMMMMMMMWk.                      .oNMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0;                    'kWMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNk;.               ,dXMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0dc,..    ..':oONMMMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0OOOO0KNMMMMMMMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMWKkxOXWMMMMMMMMMMWXOxkXWMMMMMMMMMMMWN0xkKWMMMMMMMMMMMN0kk0NMMMMMMMMMMMMMN:        
+   .xMMMMMMMMWXOdc,.   .;lx0NMMN0xl;.   .,cdOXWMNKOkl;.   .,cdOXWMWKko:'.  .':okKWMMMMMMMMN:        
+   .xMMMMN0xl;.            .':;.             .;:'.             .,:,.            .,cdOXWMMMN:        
+   .xMMWO;.                                                                          .dNMMN:        
+   .xMMMNk,                                                                         .oXMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMWo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMMo                                                                         '0MMMMN:        
+   .xMMMMWo                                                                         '0MMMMN:        
+   .xMMWKo.                                                                          ;ONMMN:        
+   .xMMWOc'.                                                                        .;dXMMN:        
+   .xMMMMMNKxl;.          .,coo:'.         .':ooc,..          .;ldl;.          .,cdOXWMMMMN:        
+   .xMMMMMMMMMWN0xl;..':okKWMMMMN0xl;...;lx0NMMMMWNKko:'..,cdOXWMMMWXOdc,..'coOXWMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMWXKNMMMMMMMMMMMMMWXKXWMMMMMMMMMMMMMMNKXWMMMMMMMMMMMMMWKKNMMMMMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:        
+   .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:        
+   .dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX;        
+    'xKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXOc.        
+      ....................................................................................          
+                                                                                                    
+    
+      </pre>
+    </div>
+    
+    </div>
+    <!-- end of  -->
+    <div class="foxCont" id="foxCont">
+      <div id="output"></div>
+      <label>
+        <input type="text" id="data">
+      </label>
+    </div>
+    <!-- end of foxcont -->
+
+    <div class="imageCont">
+    </div>
+    <!-- end of  imageCont-->
+
+    <!-- FORM 1  FAVORITE HOBBY -->
+    <div class="formCont hobby">
+      <form id="hobby" action="formHandle.php" method="post">
+      <span class="russian"></span>Favorite hobby:<br><br> <input type="text" name="group1"><br>
+      <input type="submit">
+      <div class="fakeButton">GENERATE</div>
+      </form> 
+    </div>
+    <!-- end of  PERSON LOCATION FORM-->
+
+
+    <!-- FORM 2 FAVORITE THING IN THE WORLD -->
+    <div class="formCont favThing">
+      <form id="favThing" action="formHandle.php" method="post">
+      <span class="russian"></span>Favorite thing in the world:<br><br> <input type="text" name="group1"><br>
+      <input type="submit">
+      <div class="fakeButton">GENERATE</div>
+      </form> 
+    </div>
+    <!-- end of  PERSON LOCATION FORM-->
+</div>
+<!-- end of  mainCont-->
+
+
+<!-- include the ajax form submit and backgroundremove API -->
+<script type="text/javascript" src="js/formAjax.js"></script>
+
+<!-- include the CORS bypass script -->
+<script type="text/javascript" src="js/cors.js"></script>
+
+</body>
+</html>
